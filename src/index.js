d31da88b24010e4857cae39a72bbde02f90e6812
Andrey <prog.qt@gmail.com>
2021-03-10 23:48:37 +0300

    part 2 - 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import HeaderBlock from "./components/HeaderBlock";
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const AppList = () => {
+    const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+    const firstItem = <li>Item 0</li>;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const isAuth = false;
+
+    return (
+        <ul>
+            { isAuth ? firstItem : null }
+            { items.map(item => <li>{item}</li>) }
+            <li>{ items[0] }</li>
+            <li>{ items[1] }</li>
+        </ul>
+    );
+}
+
+const AppHeader = () => {
+    /*const margin = 40;
+    const headerStyle = {
+        color: 'red',
+        marginLeft: `${margin}px`,
+        marginBottom: `${margin}px`
+    };*/
+    return (
+        <h1
+            // style={headerStyle}
+            className="header"
+        >
+            Hello World, React.js!
+        </h1>);
+}
+
+const AppInput = () => {
+    const placeholder = 'Type text';
+    return (
+        <label>
+            <input type="text" placeholder={placeholder} />
+        </label>
+    );
+}
+
+const App = () => {
+    return (
+        <>
+            <HeaderBlock/>
+            <AppHeader/>
+            <AppInput/>
+            <AppList/>
+            <AppHeader/>
+            <AppList/>
+        </>
+    );
+}
+
+/*const el = (
+    <App/>
+);*/
+
+ReactDOM.render(<App/>, document.getElementById('root'));
